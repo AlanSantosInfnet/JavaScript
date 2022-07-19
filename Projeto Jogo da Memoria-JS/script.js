0ba12bc468fac943fea6305b7e1f7fe529e6a0e6
Alan Santos <alan.santos@al.infnet.edu.br>
2022-07-19 12:45:56 -0300

    Projeto Jgo da Memoria-JS

---
+++ Projeto Jogo da Memoria-JS/script.js
@@ -0,0 +1,150 @@
+let cards = ['dog1.png', 'dog1.png', 'dog2.png', 'dog2.png', 'dog3.png', 'dog3.png', 'dog4.png', 'dog4.png', 'dog5.png', 'dog5.png', 'dog6.png', 'dog6.png', 'dog7.png', 'dog7.png', 'dog8.png', 'dog8.png']
+let gameBoard = document.getElementById('tabuleiro')
+let chronometer = document.getElementById('chrono')
+let highscores = document.getElementById('scores')
+
+let blockClicks = false
+let score = 0
+let turnedCards = []
+
+let seconds = 0
+let minutes = 0
+let displaySeconds = 0
+let displayMinutes = 0
+let interval = null
+
+function gameStart(){
+
+    gameBoard.innerHTML = ``
+    window.clearInterval(interval)
+    score = 0
+
+    cards.forEach(card => {
+        gameBoard.innerHTML += 
+            `<div class="gameCard" data-value="${card}">
+                <img class="frontFace" src="img/${card}">
+            </div>`
+    })
+
+    setTimeout(() => {
+        shuffleCards()
+        let reset = document.getElementById("startBtn")
+        reset.innerText = "Reiniciar Jogo"
+    }, 3000)
+}
+
+function shuffleCards(){
+
+    cards.sort(()=> (Math.random() > .5) ? 1 : -1)
+
+    gameBoard.innerHTML = ``
+    cards.forEach((card, i) => {
+        gameBoard.innerHTML += 
+            `<div class="gameCard">
+                <img class="frontFace" data-value="${card}" src="img/${card}">
+            </div>`
+    })
+
+    gameIsGo()
+}
+
+function gameIsGo(){
+
+    let initialCards = document.getElementsByClassName('frontFace')
+
+    setTimeout(() => {
+        for (let i = 0; i < initialCards.length; i++){
+            let card = initialCards[i];
+            card.classList.toggle('turned')
+        }
+
+        seconds = 0
+        minutes = 0
+        interval = window.setInterval(startChrono, 1000)
+        startChrono()
+    }, 1500)
+
+    let gameCard = document.querySelectorAll(".gameCard img")
+    
+    gameCard.forEach((card, i) => {
+        card.addEventListener("click", turnCard)
+    })
+}
+
+function startChrono(){
+
+    seconds++
+
+    if(seconds /60 === 1){
+        seconds = 0
+        minutes++
+    }
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString()
+    } else {
+        displaySeconds = seconds
+    }
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString()
+    } else {
+        displayMinutes = minutes
+    }
+
+    chronometer.innerHTML = displayMinutes + ":" + displaySeconds
+}
+
+function turnCard(card){
+
+    if(blockClicks){
+        return
+    }
+
+    card.target.classList.toggle('turned')
+    card.target.classList.add('selected')
+    card.target.removeEventListener("click", turnCard)
+    let turned = card.target.getAttribute('data-value')
+    turnedCards.push(turned)
+
+    if(turnedCards.length === 2){
+
+        blockClicks = true
+
+        if(turnedCards[0] === turnedCards[1]){
+            score++
+            console.log(score);
+            blockClicks = false
+            turnedCards = []
+
+            let selectedCards = document.querySelectorAll("img.frontFace.selected")
+            selectedCards.forEach(card =>
+                card.classList.remove('selected')
+            )
+
+            if(score === 8){
+                gameEnd()
+            }
+
+        } else {
+            let selectedCards = document.querySelectorAll("img.frontFace.selected")
+
+            selectedCards.forEach(card =>
+                setTimeout(() => {
+                    card.classList.remove('selected')
+                    card.addEventListener("click", turnCard)
+                    card.classList.toggle('turned')
+                    turnedCards = []
+                    blockClicks = false    
+                    selectedCards = []}
+                , 1500)
+            )
+        }
+    }
+
+}
+
+function gameEnd(){
+    window.clearInterval(interval)
+    window.alert(`Parabéns! Seu tempo final é ${displayMinutes}:${displaySeconds}`)
+    let userName = window.prompt("Digite seu nome:")
+    highscores.innerHTML += `<li><strong>${userName}</strong> - ${displayMinutes}:${displaySeconds}</li>`
+}
